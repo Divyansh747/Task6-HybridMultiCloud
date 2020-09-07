7291b4dd7aff3696ddd36b71a47180f75d9ee9ba
Divyansh747 <divyansh.1999.dr@gmail.com>
2020-09-08 01:36:45 +0530

    add aws db instance

--- main.tf
+++ main.tf
@@ -3,4 +3,16 @@ provider "aws" {
   profile = "default"
 }
 
-
+resource "aws_db_instance" "rds" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7.30"
+  instance_class       = "db.t2.micro"
+  name                 = "wordpress"
+  username             = "wordpress"
+  password             = "wordpress"
+  parameter_group_name = "default.mysql5.7"
+  publicly_accessible  = "true"
+  port                 = "3306"
+}
